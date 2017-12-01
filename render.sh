ef047625a86650a5dbdb3f9288ad3a75d03468be
Niklas Wagner <Skaro@Skaronator.com>
2017-12-01 13:24:17 +0100

    Add Echo

--- render.sh
+++ render.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 if [ ! -f /config/render.conf ]; then
+	echo "render.conf not found, creating default render.conf!"
     cp /render.conf /config/render.conf
 fi
+
+echo "Starting mapcrafter with $THREADS threads!"
 
 mapcrafter -c /config/render.conf -j $THREADS >> /var/log/cron.log 2>&1
